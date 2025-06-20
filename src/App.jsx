3b7cb70e4b1992561ae7aac1c2c03dc2f6327c58
Salvatore <workstudioms@gmail.com>
2025-06-20 13:47:49 +0200

    feat: Milestone 2 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 
 import "./App.css"
 
 function App() {
 
   const [politicians, setPoliticians] = useState([])
+
+  const [search, setSearch] = useState("")
 
   useEffect(() => {
     fetch("http://localhost:3333/politicians")
@@ -14,18 +16,33 @@ function App() {
 
   }, []);
 
-  console.log(politicians)
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+
+  const filteredPoliticians = useMemo(() => {
+    return politicians.filter(politician => {
+      const isNameIncluded = politician.name.toLowerCase().includes(search.toLowerCase())
+      const isBioIncluded = politician.biography.toLowerCase().includes(search.toLowerCase())
+      return isNameIncluded || isBioIncluded;
+    })
+  }, [politicians, search])
 
   return (
     <>
+      <input type="text" value={search} onChange={handleChange} />
+
       <h1>Lista Politici</h1>
       <div className="politicians-list">
-        {politicians.map((politician) => <div className="card" key={politician.id}>
-          <img src={politician.image} alt={politician.name} />
-          <h2>{politician.name}</h2>
-          <p>{politician.position}</p>
-          <p>{politician.biography}</p>
-        </div>)}
+        {filteredPoliticians.map((politician) =>
+          <div className="card" key={politician.id}>
+            <img src={politician.image} alt={politician.name} />
+            <h2>{politician.name}</h2>
+            <p>{politician.position}</p>
+            <p>{politician.biography}</p>
+          </div>)}
       </div>
     </>
   )
