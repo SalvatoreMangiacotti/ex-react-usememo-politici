ac66d6e919c00d6e053dab70b79f978baed9b67a
Salvatore <workstudioms@gmail.com>
2025-06-20 15:01:00 +0200

    feat: Milestone 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
-import { useState, useEffect, useMemo } from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 
 import "./App.css"
 
+function PoliticianCard({ name, image, position, biography }) {
+  console.log("Card")
+  return (
+    <div className="card">
+      <img src={image} alt={name} />
+      <h2>{name}</h2>
+      <p>{position}</p>
+      <p>{biography}</p>
+    </div>
+  );
+}
+
+const MemoPoliticianCard = React.memo(PoliticianCard); // Quando vogliamo avere entrambe le versioni e non solo quella "memoizata", 
+// altrimenti si può passare direttamente a PoliticianCard
+
 function App() {
 
-  const [politicians, setPoliticians] = useState([])
+  const [politicians, setPoliticians] = useState([]);
 
-  const [search, setSearch] = useState("")
+  const [search, setSearch] = useState('');
+
 
   useEffect(() => {
-    fetch("http://localhost:3333/politicians")
-      .then(response => response.json())
-      .then(data => setPoliticians(data))
-      .catch(error => console.error(error))
-
+    fetch('http://localhost:3333/politicians')
+      .then((response) => response.json())
+      .then((data) => setPoliticians(data))
+      .catch((error) => console.error(error));
   }, []);
 
 
@@ -23,30 +38,36 @@ function App() {
 
 
   const filteredPoliticians = useMemo(() => {
-    return politicians.filter(politician => {
-      const isNameIncluded = politician.name.toLowerCase().includes(search.toLowerCase())
-      const isBioIncluded = politician.biography.toLowerCase().includes(search.toLowerCase())
+    return politicians.filter((politician) => {
+      const isNameIncluded = politician.name
+        .toLowerCase()
+        .includes(search.toLowerCase());
+      const isBioIncluded = politician.biography
+        .toLowerCase()
+        .includes(search.toLowerCase());
       return isNameIncluded || isBioIncluded;
-    })
-  }, [politicians, search])
+    });
+  }, [politicians, search]);
+
 
   return (
     <>
-      <input type="text" value={search} onChange={handleChange} />
+      <input
+        type="text"
+        value={search}
+        onChange={handleChange}
+        placeholder="Cerca per nome o biografia"
+      />
 
       <h1>Lista Politici</h1>
       <div className="politicians-list">
-        {filteredPoliticians.map((politician) =>
-          <div className="card" key={politician.id}>
-            <img src={politician.image} alt={politician.name} />
-            <h2>{politician.name}</h2>
-            <p>{politician.position}</p>
-            <p>{politician.biography}</p>
-          </div>)}
+        {filteredPoliticians.map((politician) => (
+          <MemoPoliticianCard key={politician.id} {...politician} />
+        ))}
       </div>
     </>
-  )
-
+  );
 }
 
-export default App
+
+export default App;
